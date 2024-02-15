c173b76b97e33515912f02a53241748380db9a0f
Kay Bee <77047528+kengboonang@users.noreply.github.com>
2024-02-15 23:20:51 +0800

    [feat] Adding respective schemas and openai class to the project

--- src/chatgpt.py
+++ src/chatgpt.py
@@ -1,28 +1,40 @@
 # Description: This file contains the ChatGPT class which is used to interact with the OpenAI GPT-3.5 API.
+import json
 import openai
 import os
+import requests
+from time import sleep
 from dotenv import load_dotenv
 
 load_dotenv()
 
-def check_openai_api_key(api_key):
-    openai.api_key = api_key
-    try:
-        openai.Model.list()
-    except openai.error.AuthenticationError as e:
-        return False
-    else:
-        return True
+class ChatGPT(object):
+    def __init__(self, num_tries=1):
+        self.client = openai.OpenAI(api_key=os.getenv("OPENAI_KEY"))
+        self.num_tries = num_tries
 
-# class ChatGPT(object):
-#     def __init__(self, api_key):
-#         openai.api_key = api_key
+    def generate(self, role, prompt):
+        is_done = False
+        num_tries = self.num_tries
+        response_str = "Error"
+        while not is_done and num_tries > 0:
+            try:
+                response = self.client.chat.completions.create(
+                    model="gpt-3.5-turbo",
+                    messages=[{"role": role, "content": prompt}],
+                    max_tokens=150,
+                    temperature=1.0,
+                )
+                response_str = response.choices[0].message.content
+                is_done = True
+            except Exception as err:
+                # retry in 5 seconds in case of network error
+                print(f"Error: {err}")
+                num_tries -= 1
+                sleep(5)
+                pass
+        return response_str
 
 if __name__ == "__main__":
-    api_key = os.getenv("OPENAI_KEY")
-    is_valid = check_openai_api_key(api_key)
-
-    if is_valid:
-        print("Valid OpenAI API key.")
-    else:
-        print("Invalid OpenAI API key.")
+    chat = ChatGPT()
+    print(chat.generate("What is the meaning of life?"))
